1c0d8c5c7a327208865e6f3100ac870f5f888bab
Theodoros90 <ionas90@hotmail.com>
2025-09-26 17:45:15 +0300

    2- Adding Client first commit

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,10 @@
+type Customer = {
+    id: string
+    firstName: string
+    lastName: string
+    phoneNumber: string
+    email: string
+    description: string
+    createdAt: string
+    isActive: boolean
+}
